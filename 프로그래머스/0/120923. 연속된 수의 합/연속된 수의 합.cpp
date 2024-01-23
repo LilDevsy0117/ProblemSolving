af493e9d84fd710a51c7d4a7f30a7c5b66d8a877
SangYeop Lee <skkulsy0117@naver.com>
2024-01-24 00:53:49 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int num, int total) {
+    vector<int> answer;
+    int d = 0;
+    for(int i = 1; i<num; i++){
+        d += i;
+    }
+    int a = (total - d)/num;
+    for(int i =0; i<num; i++){
+        answer.push_back(a + i);
+    }
+    return answer;
+}
