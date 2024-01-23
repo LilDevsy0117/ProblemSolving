8f80fcdc48cd2d70c6c643630dbc92252b19d340
SangYeop Lee <skkulsy0117@naver.com>
2024-01-23 16:09:21 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    stringstream ss(my_string);
+    string word;
+    while(getline(ss, word, ' ')){
+        answer.push_back(word);
+    }
+    return answer;
+}
