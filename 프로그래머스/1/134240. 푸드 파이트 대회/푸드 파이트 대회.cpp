7615e5613ea99e776f9e1aaad045daef74a110c4
SangYeop Lee <skkulsy0117@naver.com>
2024-01-24 02:31:12 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    for(int i = 1; i<food.size(); i++){
+        for(int j = 0; j<food[i]/2; j++){
+            answer += to_string(i);
+        }
+    }
+    string s =answer;
+    reverse(s.begin(), s.end());
+    answer = answer + "0" + s;
+    return answer;
+}
