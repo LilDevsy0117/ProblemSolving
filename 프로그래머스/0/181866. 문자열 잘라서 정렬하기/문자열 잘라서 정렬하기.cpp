6750afe9f197bc088c6f98f3d63bec99924aefa5
SangYeop Lee <skkulsy0117@naver.com>
2024-01-23 17:48:19 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 6.55 ms, Memory: 6.43 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<string> solution(string myString) {
+    vector<string> answer;
+    string word = "";
+    for(int i = 0; i<myString.size(); i++){
+        if(myString[i] == 'x'){
+            if(word.size()>0){
+                answer.push_back(word);
+            }
+            word = "";
+        }else{
+            word += myString[i];
+        }
+    }
+    if(word.size()>0){
+        answer.push_back(word);
+    }
+    
+    sort(answer.begin(), answer.end());
+    return answer;
+}
