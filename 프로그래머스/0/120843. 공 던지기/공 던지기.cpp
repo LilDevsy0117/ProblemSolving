cc7cfb5d4aab291d0c2725d948d450c1e8ee408f
SangYeop Lee <skkulsy0117@naver.com>
2024-01-23 20:21:43 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int k) {
+    int answer = 0;
+    int a = 1;
+    while(a < k){
+        answer += 2;
+        answer %= numbers.size();
+        a++;
+    }
+
+    return answer+1;
+}
