29b7b5852cbaec59c529b5dcc2b4de3b89f5fae1
SangYeop Lee <skkulsy0117@naver.com>
2024-01-23 16:08:43 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(int i =0; i<myString.size(); i++){
+        if(myString[i] != 'a' && myString[i] != 'A'){
+            myString[i] = tolower(myString[i]);
+        }else if(myString[i] == 'a'){
+            myString[i] = toupper(myString[i]);
+        }
+    }
+    return myString;
+}
