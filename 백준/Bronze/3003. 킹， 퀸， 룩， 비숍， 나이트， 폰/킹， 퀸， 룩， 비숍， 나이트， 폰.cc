0289c0b1be46c0078cd2276b6ac3f62d5ef71220
SangYeop Lee <skkulsy0117@naver.com>
2024-01-31 02:27:27 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int a,b,c,d,e, f;
+	cin>>a>>b>>c>>d>>e>>f;
+	
+	cout<<1-a<<" "<<1-b<<" "<<2-c<<" "<<2-d<<" "<<2-e<<" "<<8-f<<" ";
+	
+	
+	return 0;
+}
