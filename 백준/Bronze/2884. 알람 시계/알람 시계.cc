d07c2e714236b0d0757de13ccc7c12720a51d154
SangYeop Lee <skkulsy0117@naver.com>
2024-01-30 16:50:22 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int H,M;
+	cin>>H>>M;
+	if(M<45){
+		M+=60;
+		M-=45;
+		if(H==0){
+			H=23;
+		}else{
+			H-=1;
+		}
+		
+	}else{
+		M-=45;
+	}
+	cout<<H<<" "<<M<<endl;
+	
+}
