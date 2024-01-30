1b3edd0f9e09788101afdff4fe0a7384ce3a3e0f
SangYeop Lee <skkulsy0117@naver.com>
2024-01-31 01:34:02 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	string s;
+	cin>>s;
+	
+	cout<<s.length();
+	
+	return 0;
+}
