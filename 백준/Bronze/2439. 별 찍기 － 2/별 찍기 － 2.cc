43d40f3c7c3fbf94091b4a15df845aa4c417f1d7
SangYeop Lee <skkulsy0117@naver.com>
2024-01-30 18:23:10 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	// cout.tie(NULL);
+
+	int n;
+	cin>>n;
+	
+	for(int i =0; i<n; ++i){
+		for(int k = 1; k<n-i; ++k){
+			cout<<" ";
+		}
+		for(int j = 0 ;j<=i; ++j){
+			cout<<"*";
+		}
+		cout<<"\n";
+	}
+
+	
+}	
