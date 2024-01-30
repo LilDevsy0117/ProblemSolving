0c260bd3a5b40dcdcc94b4b711aca6173ccd1abe
SangYeop Lee <skkulsy0117@naver.com>
2024-01-30 18:24:13 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	// cout.tie(NULL);
+
+	int n1, n2;
+	while(cin >> n1 >>n2){
+		cout<< n1 + n2 <<"\n";
+	}
+
+	
+}	
