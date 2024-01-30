4a4c4efd23f1af7905405b4ac56dfd83418626e3
SangYeop Lee <skkulsy0117@naver.com>
2024-01-30 18:17:26 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n;
+	cin>>n;
+	
+	for(int i =0; i<n/4; ++i){
+		cout<<"long ";
+		
+	}
+	
+	
+	
+	cout<<"int";
+	
+	return 0;
+}
