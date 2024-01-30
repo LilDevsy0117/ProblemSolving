e56244b51741dda04a4d4aba28709ec59022e584
SangYeop Lee <skkulsy0117@naver.com>
2024-01-31 01:38:18 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	string s;
+	while(getline(cin ,s)){
+		cout<<s<<"\n";
+	}
+	
+	
+	return 0;
+}
