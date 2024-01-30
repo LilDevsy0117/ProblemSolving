39cb97ebc8f98f583dc2ee1d06e83f48a54d8fb8
SangYeop Lee <skkulsy0117@naver.com>
2024-01-31 01:35:06 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	// cout.tie(NULL);
+	
+	char a;
+	cin>>a;
+	cout<<static_cast<int>(a)<<"\n";
+	
+	
+	return 0;
+}
+
