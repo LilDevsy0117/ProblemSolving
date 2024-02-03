75835ee9761278b18a93e1327155da521857eaaf
SangYeop Lee <skkulsy0117@naver.com>
2024-02-03 23:13:49 +0900

    Update 저주의 숫자 3.cpp

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -7,6 +7,7 @@ int solution(int n) {
     int answer = 0;
     for(int i =1;i<=n; i++){
         answer++;
+        # 3으로 나누어지거나, 3이 포함되거나 한 경우 제외
         while(answer % 3 == 0 || to_string(answer).find("3") != string::npos){
             answer++;
         }
