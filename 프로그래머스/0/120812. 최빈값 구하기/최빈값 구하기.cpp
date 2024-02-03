f812ff89824afa3bbfe06aac6681c8f085b7e67c
SangYeop Lee <skkulsy0117@naver.com>
2024-02-03 23:15:55 +0900

    Update 최빈값 구하기.cpp

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cpp
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int solution(vector<int> array) {
     int answer = 0;
     vector<int> v(1000);
+    # array 에 값에 해당하는 벡터 인덱스 참조 후 값 증가
     for(int i =0; i<array.size(); i++){
         v[array[i]]++;
     }
     int max = *max_element(v.begin(), v.end());
+    # 최빈값 구하고 최빈값이 개수가 하나 이상이면 -1
     if(count(v.begin(), v.end(), max) >1){
         answer = -1;
     }else{
