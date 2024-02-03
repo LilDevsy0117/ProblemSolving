1cfad0b25ed287fce68cd48a4b226533fd7fa553
SangYeop Lee <skkulsy0117@naver.com>
2024-02-03 23:20:04 +0900

    Update 겹치는 선분의 길이.cpp

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cpp
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int solution(vector<vector<int>> lines) {
     int answer = 0;
     vector<int> v(201);
+    # 벡터 선언해서 [x,y)범위로 값 1증가
     for(int i =0; i<lines.size(); i++){
         for(int j =lines[i][0]; j<lines[i][1]; j++){
             v[j+100]++;
