4b168c06c0c1abde7c0a733c8431debd15ad3cc4
SangYeop Lee <skkulsy0117@naver.com>
2024-02-08 01:36:16 +0900

    [level 2] Title: 행렬의 곱셈, Time: 0.97 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    for(int i = 0; i<arr1.size(); i++){
+        vector<int> v;
+        for(int k = 0; k<arr2[0].size(); k++){
+            int a = 0;
+            for(int j = 0; j<arr1[i].size(); j++){
+                a+= arr1[i][j] * arr2[j][k];
+            }
+            v.push_back(a);
+        }
+        answer.push_back(v);
+    }
+    return answer;
+}
