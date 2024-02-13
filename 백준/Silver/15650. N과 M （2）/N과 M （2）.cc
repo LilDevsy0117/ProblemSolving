13cb7abc03b68decb4438fb08ed2dd7c089991eb
SangYeop Lee <skkulsy0117@naver.com>
2024-02-13 11:52:44 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+
+using namespace std;
+int n,m;
+int arr[9] = {0,};
+bool visited[9] = {0, };
+
+void dfs(int cnt){
+	
+	if(cnt == m){
+		for(int i=0;i<m;i++){
+			cout<<arr[i]<<" ";
+		}
+		cout<<"\n";
+		return;
+	}
+	
+	for(int i=1; i<=n; ++i){
+		if(!visited[i] && i>arr[cnt-1]){
+			visited[i] = true;
+			arr[cnt] = i;
+			dfs(cnt +1);
+			visited[i] = false;
+		}
+	}
+}
+
+
+int main() {	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin>>n>>m;
+	dfs(0);
+	
+	return 0;
+}
