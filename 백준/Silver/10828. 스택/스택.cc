1a82061c1f6b7e3b01eeceb249eb74f2c874e6d0
SangYeop Lee <skkulsy0117@naver.com>
2024-03-09 11:22:30 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <set>
+#include <map>
+#include <stack>
+#include <numeric>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    stack<int> st;
+
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        if (s == "push")
+        {
+            int a;
+            cin >> a;
+            st.push(a);
+        }
+        else if (s == "top")
+        {
+            if (st.size() == 0)
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << st.top() << "\n";
+            }
+        }
+        else if (s == "pop")
+        {
+            if (st.size() == 0)
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << st.top() << "\n";
+                st.pop();
+            }
+        }
+        else if (s == "size")
+        {
+            cout << st.size() << "\n";
+        }
+        else if (s == "empty")
+        {
+            if (st.empty())
+            {
+                cout << 1 << "\n";
+            }
+            else
+            {
+                cout << 0 << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
