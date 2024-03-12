7e8c54a0a901d9ca6dd1d2733e3b277b47613c13
SangYeop Lee <skkulsy0117@naver.com>
2024-03-12 19:56:39 +0900

    [Silver III] Title: N과 M (7), Time: 492 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <set>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <queue>
+#include <deque>
+#include <sstream>
+#include <list>
+using namespace std;
+
+int n, m;
+int MAX = 8;
+vector<int> k(MAX);
+vector<int> v;
+vector<bool> visited(MAX);
+void go(int z)
+{
+    if (z == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << k[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        k[z] = v[i];
+        go(z + 1);
+        // if (!visited[i])
+        // {
+        //     visited[i] = true;
+        //     if (z > 0 && k[z - 1] < v[i])
+        //     {
+        //         k[z] = v[i];
+        //         go(z + 1);
+        //     }
+        //     else if (z == 0)
+        //     {
+        //         k[z] = v[i];
+        //         go(z + 1);
+        //     }
+        //     visited[i] = false;
+        // }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    v.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    go(0);
+
+    return 0;
+}
