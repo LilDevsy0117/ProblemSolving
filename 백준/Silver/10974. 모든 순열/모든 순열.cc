c3243958408cafcf75fe25125f6bbbde2679385c
SangYeop Lee <skkulsy0117@naver.com>
2024-03-19 17:32:18 +0900

    [Silver III] Title: 모든 순열, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        v[i] = i + 1;
+    }
+    do
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+    } while (next_permutation(v.begin(), v.end()));
+
+    return 0;
+}
