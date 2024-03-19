fcbe003f1772a58d90e8284c00fb1de7903a7715
SangYeop Lee <skkulsy0117@naver.com>
2024-03-19 17:28:07 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    if (prev_permutation(v.begin(), v.end()))
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << v[i] << " ";
+        }
+    }
+    else
+    {
+        cout << -1;
+    }
+
+    return 0;
+}
