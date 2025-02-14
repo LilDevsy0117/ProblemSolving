b4c9fc0a0f421c5ac3a7d7a2d4a81dbe6e5faa76
SangYeop Lee <skkulsy0117@naver.com>
2025-02-14 20:58:25 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,69 +1,56 @@
-	#include <iostream>
-	#include <vector>
-	#include <algorithm>
-	#include <string>
-	#include <cmath>
-	#include <set>
-	#include <map>
-	#include <queue>
-	#include <string.h>
-	#include<stack>
-	#include<climits>
-	using namespace std;
-	typedef long long ll;
-	const int INF = INT_MAX;
-	
-	int V, E;
-	vector<pair<int,int>> edge[20001];
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> Q;
-	int dist[20001];
-	bool visit[20001];
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
 
-	int main()
+using namespace std;
+typedef long long ll;
+const int INF = 987654321;
+int V, E, K;
+
+vector<pair<int, int>> edge[20001];
+priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>> Q;
+int dist[20001];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	cin >> V >> E >> K;
+
+	for (int i = 1; i <= E; i++)
 	{
-		ios::sync_with_stdio(false );
-		cin.tie(NULL);
-		cout.tie(NULL);
-	
-		cin >> V >> E;
-		int K;
-		cin >> K;
-		Q.push({ 0,K });
-		fill(dist, dist + 20001, INF);
-		dist[K] = 0;
-
-		for (int i = 0; i < E; i++)
+		int u, v, w;
+		cin >> u >> v >> w;
+		edge[u].push_back({ w,v });
+	}
+	fill(dist, dist + 20001, INF);
+	dist[K] = 0;
+	Q.push({ 0,K });
+	while (!Q.empty())
+	{
+		int cur = Q.top().second;
+		int curD = Q.top().first;
+		Q.pop();
+		for (auto it : edge[cur])
 		{
-			int u, v, w;
-			cin >> u >> v >> w;
-			edge[u].push_back({ w,v });
-		}
-
-		while (!Q.empty())
-		{
-			int cur = Q.top().second;
-			int curD = Q.top().first;
-			Q.pop();
-
-			if (visit[cur]) continue;
-			visit[cur] = true;
-
-			for (auto it : edge[cur])
+			int nextD = it.first;
+			int next = it.second;
+			if (dist[next] > curD + nextD)
 			{
-				if (dist[it.second] > curD + it.first)
-				{
-					dist[it.second] = curD + it.first;
-					Q.push({ curD + it.first ,it.second	});
-				}
+				dist[next] = curD + nextD;
+				Q.push({ dist[next], next });
 			}
 		}
-
-		for (int i = 1; i <= V; i++)
-		{
-			if (dist[i] == INF) cout << "INF\n";
-			else cout << dist[i] << "\n";
-		}
-
-
-		return 0;
 	}
+	for (int i = 1; i <= V; i++)
+	{
+		if (dist[i] == INF) cout << "INF\n";
+		else cout << dist[i] << "\n";
+	}
+
+	return 0;
+}
